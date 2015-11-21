c2669872cd05a48f9fb467331f0ccef4f4862e42
dg-w36 <davidwhq123@gmail.com>
2015-11-21 23:52:31 +0800

    fix some bugs

--- code/special_color.cpp
+++ code/special_color.cpp
@@ -20,200 +20,227 @@ Point origin;
 Rect selection;
 int vmin = 10, vmax = 256, smin = 30;
 
+static void onMouse( int event, int x, int y, int, void* )
+{
+    if( selectObject )
+    {
+        selection.x = MIN(x, origin.x);
+        selection.y = MIN(y, origin.y);
+        selection.width = std::abs(x - origin.x);
+        selection.height = std::abs(y - origin.y);
 
+        selection &= Rect(0, 0, image.cols, image.rows);
+    }
 
-static void onMouse( int event, int x, int y, int, void* ){
-  if( selectObject ){
-    selection.x = MIN(x, origin.x);
-    selection.y = MIN(y, origin.y);
-    selection.width = std::abs(x - origin.x);
-    selection.height = std::abs(y - origin.y);
-
-    selection &= Rect(0, 0, image.cols, image.rows);
-  }
-
-  switch( event ){
-  case EVENT_LBUTTONDOWN:
-    origin = Point(x,y);
-    selection = Rect(x,y,0,0);
-    selectObject = true;
-    break;
-  case EVENT_LBUTTONUP:
-    selectObject = false;
-    if( selection.width > 0 && selection.height > 0 )
-      trackObject = -1;
-    break;
-  }
+    switch( event )
+    {
+    case EVENT_LBUTTONDOWN:
+        origin = Point(x,y);
+        selection = Rect(x,y,0,0);
+        selectObject = true;
+        break;
+    case EVENT_LBUTTONUP:
+        selectObject = false;
+        if( selection.width > 0 && selection.height > 0 )
+            trackObject = -1;
+        break;
+    }
 }
 
-static void help(){
-  /*    cout << "\nThis is a demo that shows mean-shift based tracking\n"
-	"You select a color objects such as your face and it tracks it.\n"
-	"This reads from video camera (0 by default, or the camera number the user enters\n"
-	"Usage: \n"
-	"   ./camshiftdemo [camera number]\n";
-  */
-  cout << "\n\nHot keys: \n"
-    "\tESC - quit the program\n"
-    "\tc - stop the tracking\n"
-    "\tb - switch to/from backprojection view\n"
+static void help()
+{
+    cout << "\nThis is a demo that shows mean-shift based tracking\n"
+            "You select a color objects such as your face and it tracks it.\n"
+            "This reads from video camera (0 by default, or the camera number the user enters\n"
+            "Usage: \n"
+            "   ./camshiftdemo [camera number]\n";
 
-    "\tp - pause video\n"
-    "To initialize tracking, select the object with mouse\n";
+    cout << "\n\nHot keys: \n"
+            "\tESC - quit the program\n"
+            "\tc - stop the tracking\n"
+            "\tb - switch to/from backprojection view\n"
+            "\th - show/hide object histogram\n"
+            "\tp - pause video\n"
+            "To initialize tracking, select the object with mouse\n";
 }
 
 const char* keys =
-  {
+{
     "{@camera_number| 0 | camera number}"
-  };
+};
 
-int main( int argc, const char** argv ){
-  help();
-
-  VideoCapture cap;
-  Rect trackWindow;
-
-  int hsize = 16;
-  float hranges[] = {0,180};
-
-  const float* phranges = hranges;
-  CommandLineParser parser(argc, argv, keys);//
-  int camNum = parser.get<int>(0);
-
-  cap.open(camNum);
-
-  if( !cap.isOpened() ){
+int main( int argc, const char** argv )
+{
     help();
-    cout << "***Could not initialize capturing...***\n";
-    cout << "Current parameter's value: \n";
-    parser.printMessage();
-    return -1;
-  }
 
-  //    namedWindow( "Histogram", 0 );
-  namedWindow( "Special Color", 0 );
-  setMouseCallback( "CamShift Demo", onMouse, 0 );
-  createTrackbar( "Vmin", "CamShift Demo", &vmin, 256, 0 );
-  createTrackbar( "Vmax", "CamShift Demo", &vmax, 256, 0 );
-  createTrackbar( "Smin", "CamShift Demo", &smin, 256, 0 );
+    VideoCapture cap;
+    Rect trackWindow;
+    int hsize = 16;
+    float hranges[] = {0,180};
+    const float* phranges = hranges;
+    CommandLineParser parser(argc, argv, keys);
+    int camNum = parser.get<int>(0);
 
-  Mat frame, hsv, hue, mask, hist, backproj;
-  bool paused = false;
+    cap.open(camNum);
 
-  for(;;){
-    if( !paused ){
-      cap >> frame;
-      if( frame.empty() )
-	break;
+    if( !cap.isOpened() )
+    {
+        help();
+        cout << "***Could not initialize capturing...***\n";
+        cout << "Current parameter's value: \n";
+        parser.printMessage();
+        return -1;
     }
 
-    frame.copyTo(image);
+    namedWindow( "Histogram", 0 );
+    namedWindow( "CamShift Demo", 0 );
+    setMouseCallback( "CamShift Demo", onMouse, 0 );
+    createTrackbar( "Vmin", "CamShift Demo", &vmin, 256, 0 );
+    createTrackbar( "Vmax", "CamShift Demo", &vmax, 256, 0 );
+    createTrackbar( "Smin", "CamShift Demo", &smin, 256, 0 );
 
-    if( !paused ){
-      cvtColor(image, hsv, COLOR_BGR2HSV);
+    Mat frame, hsv, hue, mask, hist, histimg = Mat::zeros(200, 320, CV_8UC3), backproj;
+    bool paused = false;
 
-      if( trackObject ){
-	int _vmin = vmin, _vmax = vmax;
+    for(;;)
+    {
+        if( !paused )
+        {
+            cap >> frame;
+            if( frame.empty() )
+                break;
+        }
 
-	inRange(hsv, Scalar(0, smin, MIN(_vmin,_vmax)),
-		Scalar(180, 256, MAX(_vmin, _vmax)), mask);
+        frame.copyTo(image);
 
-	//imshow("debug", mask);
-	int ch[] = {0, 0};
-	hue.create(hsv.size(), hsv.depth());
-	mixChannels(&hsv, 1, &hue, 1, ch, 1);   //?????
+        if( !paused )
+        {
+            cvtColor(image, hsv, COLOR_BGR2HSV);
 
-	if( trackObject < 0 ){
-	  Mat roi(hue, selection), maskroi(mask, selection);
-	  calcHist(&roi, 1, 0, maskroi, hist, 1, &hsize, &phranges);
-	  normalize(hist, hist, 0, 255, NORM_MINMAX);
+            if( trackObject )
+            {
+                int _vmin = vmin, _vmax = vmax;
 
-	  trackWindow = selection;
-	  trackObject = 1;
+                inRange(hsv, Scalar(0, smin, MIN(_vmin,_vmax)),
+                        Scalar(180, 256, MAX(_vmin, _vmax)), mask);
+                int ch[] = {0, 0};
+                hue.create(hsv.size(), hsv.depth());
+                mixChannels(&hsv, 1, &hue, 1, ch, 1);
 
-	  /*                    Mat buf(1, hsize, CV_8UC3);
-				for( int i = 0; i < hsize; i++ )
-				buf.at<Vec3b>(i) = Vec3b(saturate_cast<uchar>(i*180./hsize), 255, 255);
-				cvtColor(buf, buf, COLOR_HSV2BGR); */
+                if( trackObject < 0 )
+                {
+                    Mat roi(hue, selection), maskroi(mask, selection);
+                    calcHist(&roi, 1, 0, maskroi, hist, 1, &hsize, &phranges);
+                    normalize(hist, hist, 0, 255, NORM_MINMAX);
 
-	}
+                    trackWindow = selection;
+                    trackObject = 1;
 
-	calcBackProject(&hue, 1, 0, hist, backproj, &phranges);
-	backproj &= mask;
-                
-	RotatedRect trackBox = CamShift(backproj, trackWindow,
-					TermCriteria( TermCriteria::EPS | TermCriteria::COUNT, 10, 1 ));
-	if( trackWindow.area() <= 1 ){
-	  int cols = backproj.cols, rows = backproj.rows, r = (MIN(cols, rows) + 5)/6;
-	  trackWindow = Rect(trackWindow.x - r, trackWindow.y - r,
-			     trackWindow.x + r, trackWindow.y + r) &
-	    Rect(0, 0, cols, rows);
-	}
+                    histimg = Scalar::all(0);
+                    int binW = histimg.cols / hsize;
+                    Mat buf(1, hsize, CV_8UC3);
+                    for( int i = 0; i < hsize; i++ )
+                        buf.at<Vec3b>(i) = Vec3b(saturate_cast<uchar>(i*180./hsize), 255, 255);
+                    cvtColor(buf, buf, COLOR_HSV2BGR);
 
-	if( backprojMode )
-	  cvtColor( backproj, image, COLOR_GRAY2BGR );
+                    for( int i = 0; i < hsize; i++ )
+                    {
+                        int val = saturate_cast<int>(hist.at<float>(i)*histimg.rows/255);
+                        rectangle( histimg, Point(i*binW,histimg.rows),
+                                   Point((i+1)*binW,histimg.rows - val),
+                                   Scalar(buf.at<Vec3b>(i)), -1, 8 );
+                    }
+                }
 
-	Rect tmp = trackBox.boundingRect();
-	rectangle( image, tmp, Scalar(0,0,255), 4,8,0 );
-				
-	Point Center(tmp.x + tmp.width/2 , tmp.y + tmp.height/2);
-	circle( image, Center,4,Scalar(0,0,255),-1,8);
-	if(Center.x > image.cols/2){
-	  if(Center.y > image.rows/2){
-	    line( image, Center, Point(image.cols*2/3, image.rows*2/3),Scalar(0,0,0),2,8);
-	  }
-	  else{
-	    line( image, Center, Point(image.cols*2/3, image.rows/3),Scalar(0,0,0),2,8);
-	  }				
-	}
-	else{
-	  if(Center.y > image.rows/2){
-	    line( image, Center, Point(image.cols/3, image.rows*2/3),Scalar(0,0,0),2,8);	
-	  }
-	  else{
-	    line( image, Center, Point(image.cols/3, image.rows/3),Scalar(0,0,0),2,8);					
-	  }
-	}
-      }
+                calcBackProject(&hue, 1, 0, hist, backproj, &phranges);
+                backproj &= mask;
+                RotatedRect trackBox = CamShift(backproj, trackWindow,
+                                    TermCriteria( TermCriteria::EPS | TermCriteria::COUNT, 10, 1 ));
+                if( trackWindow.area() <= 1 )
+                {
+                    int cols = backproj.cols, rows = backproj.rows, r = (MIN(cols, rows) + 5)/6;
+                    trackWindow = Rect(trackWindow.x - r, trackWindow.y - r,
+                                       trackWindow.x + r, trackWindow.y + r) &
+                                  Rect(0, 0, cols, rows);
+                }
+
+                if( backprojMode )
+                    cvtColor( backproj, image, COLOR_GRAY2BGR );
+            
+				//self add
+				Rect tmp = trackBox.boundingRect();
+				rectangle(image, tmp, Scalar(0, 0, 255), 4, 8, 0);
+			
+				Point Center(tmp.x + tmp.width / 2, tmp.y + tmp.height / 2);
+				circle(image, Center, 4, Scalar(0, 0, 255), -1, 8);
+				if (Center.x > image.cols / 2){
+					if (Center.y > image.rows / 2){
+						line(image, Center, Point(image.cols * 2 / 3, image.rows * 2 / 3), Scalar(0, 0, 0), 2, 8);
+					}
+					else{
+						line(image, Center, Point(image.cols * 2 / 3, image.rows / 3), Scalar(0, 0, 0), 2, 8);
+					}
+				}
+				else{
+					if (Center.y > image.rows / 2){
+						line(image, Center, Point(image.cols / 3, image.rows * 2 / 3), Scalar(0, 0, 0), 2, 8);
+					}
+					else{
+						line(image, Center, Point(image.cols / 3, image.rows / 3), Scalar(0, 0, 0), 2, 8);
+					}
+				}
+
+
+			}
+        }
+        else if( trackObject < 0 )
+            paused = false;
+
+        if( selectObject && selection.width > 0 && selection.height > 0 )
+        {
+            Mat roi(image, selection);
+            bitwise_not(roi, roi);
+        }
+
+		line(image, Point(image.cols / 3, 0), Point(image.cols / 3, image.rows), Scalar(0, 0, 0), 2, 8);
+		line(image, Point(image.cols * 2 / 3, 0), Point(image.cols * 2 / 3, image.rows), Scalar(0, 0, 0), 2, 8);
+		line(image, Point(0, image.rows / 3), Point(image.cols, image.rows / 3), Scalar(0, 0, 0), 2, 8);
+		line(image, Point(0, image.rows * 2 / 3), Point(image.cols, image.rows * 2 / 3), Scalar(0, 0, 0), 2, 8);
+
+		circle(image, Point(image.cols / 3, image.rows / 3), 4, Scalar(0, 0, 255), -1, 8);
+		circle(image, Point(image.cols * 2 / 3, image.rows / 3), 4, Scalar(0, 0, 255), -1, 8);
+		circle(image, Point(image.cols / 3, image.rows * 2 / 3), 4, Scalar(0, 0, 255), -1, 8);
+		circle(image, Point(image.cols * 2 / 3, image.rows * 2 / 3), 4, Scalar(0, 0, 255), -1, 8);
+
+        imshow( "CamShift Demo", image );
+        imshow( "Histogram", histimg );
+
+        char c = (char)waitKey(10);
+        if( c == 27 )
+            break;
+        switch(c)
+        {
+        case 'b':
+            backprojMode = !backprojMode;
+            break;
+        case 'c':
+            trackObject = 0;
+            histimg = Scalar::all(0);
+            break;
+        case 'h':
+            showHist = !showHist;
+            if( !showHist )
+                destroyWindow( "Histogram" );
+            else
+                namedWindow( "Histogram", 1 );
+            break;
+        case 'p':
+            paused = !paused;
+            break;
+        default:
+            ;
+        }
     }
-    else if( trackObject < 0 )
-      paused = false;
 
-    if( selectObject && selection.width > 0 && selection.height > 0 ){
-      Mat roi(image, selection);
-      bitwise_not(roi, roi);
-    }
-		
-    line( image , Point(image.cols/3,0) , Point(image.cols/3, image.rows), Scalar(0,0,0),2,8);
-    line( image , Point(image.cols*2/3,0) , Point(image.cols*2/3, image.rows), Scalar(0,0,0),2,8);
-    line( image , Point(0, image.rows/3) , Point(image.cols, image.rows/3), Scalar(0,0,0),2,8);
-    line( image , Point(0, image.rows*2/3) , Point(image.cols, image.rows*2/3), Scalar(0,0,0),2,8);
-
-    circle( image, Point(image.cols/3,image.rows/3),4,Scalar(0,0,255),-1,8);
-    circle( image, Point(image.cols*2/3,image.rows/3),4,Scalar(0,0,255),-1,8);
-    circle( image, Point(image.cols/3,image.rows*2/3),4,Scalar(0,0,255),-1,8);
-    circle( image, Point(image.cols*2/3,image.rows*2/3),4,Scalar(0,0,255),-1,8);
-	
-	
-    imshow( "CamShift Demo", image );
-
-    char c = (char)waitKey(10);
-    if( c == 27 )
-      break;
-    switch(c) {
-    case 'b':
-      backprojMode = !backprojMode;
-      break;
-    case 'c':
-      trackObject = 0;
-      break;
-    case 'p':
-      paused = !paused;
-      break;
-    default:
-      ;
-    }
-  }
-
-  return 0;
+    return 0;
 }
--- code/symmetry.cpp
+++ code/symmetry.cpp
@@ -7,14 +7,14 @@ using namespace std;
 
 
 
-void Check_symmetry(Mat frame);
+void Check_symmetry(Mat frame);//¼ì²â¶Ô³ÆÐÔ
 
 
 int main(){
 	VideoCapture capture;
 	Mat frame;
 	
-	capture.open(-1);
+	capture.open(0);
 	
 	while(capture.read(frame))
 	{
@@ -29,25 +29,26 @@ int main(){
 }
 
 void Check_symmetry( Mat frame ){
-	Mat frame_gray;
-	cvtColor( frame, frame_gray, COLOR_BGR2GRAY );
-	equalizeHist( frame_gray, frame_gray );
+	Mat frame_tmp;
+	equalizeHist( frame_tmp, frame_tmp );
 	
 	int count,cols,rows;
 	
 	count = 0;
-	cols = frame_gray.cols;
-	rows = frame_gray.rows;
+	frame_tmp = frame.clone();
+	cols = frame_tmp.cols;
+	rows = frame_tmp.rows;
 	
 	count = 0;
 	for (int i=0; i<rows;i++){
-		uchar* data = frame_gray.ptr<uchar>(i);
 		for(int j=0 ;j < cols/2 ;j++){	
-			count += ( (data[j]-data[cols-1-j]  < ERROR )? 1 : 0);
+			count += (frame_tmp.at<Vec3b>(i, j)[0] - frame_tmp.at<Vec3b>(i, cols - 1 - j)[0] < ERROR) ? 1 : 0;
+			count += (frame_tmp.at<Vec3b>(i, j)[1] - frame_tmp.at<Vec3b>(i, cols - 1 - j)[1] < ERROR) ? 1 : 0;
+			count += (frame_tmp.at<Vec3b>(i, j)[2] - frame_tmp.at<Vec3b>(i, cols - 1 - j)[2] < ERROR) ? 1 : 0;
 		}
 	}
 	
-	double score = 100 * ((double)count) / ((double)rows * cols / 2.0);
+	double score = 100 * ((double)count) / ((double)rows * cols * 3.0 / 2.0);
 	
 	printf("score = %lf\n",score);
 }
